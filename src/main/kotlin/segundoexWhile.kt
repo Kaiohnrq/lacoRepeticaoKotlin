95f2cfdfcbc069e124981c96081e68471efd679a
Kaio <kaio.gunz@gmail.com>
2022-08-17 12:29:43 -0300

    Todos exercicios resolvidos

--- src/main/kotlin/segundoexWhile.kt
+++ src/main/kotlin/segundoexWhile.kt
@@ -21,13 +21,40 @@ fun main(){
     var outrosCalmo = 0
     var nervosa40 = 0
     var calma18 = 0
-    var menor18 = 0
-    var maior40 = 0
 
-    while(entrevistado <= 5){
-        print("Qual a sua idade?")
+    while(entrevistado < 5){
+        print("Qual a sua idade? ")
         idade = readln().toInt()
-        print("Qual o seu sexo? (1 - F, 2 - M, 3 - Outros")
-
+        print("Qual o seu sexo? (1 - F, 2 - M, 3 - Outros) ")
+        var resp = readln()
+        print("Voce é: 1 - Calmo, 2 - Nervoso, 3 - Agressivo? ")
+        var psico = readln()
+        if(psico == "1"){
+            pessoaCalma++
+        }
+        if(resp == "1" && psico == "2"){
+            mulherNervosa++
+        }
+        if(resp == "2" && psico == "3"){
+            homemAgressivo++
+        }
+        if(resp == "3" && psico == "1"){
+            outrosCalmo++
+        }
+        if(psico == "2" && idade > 40){
+            nervosa40++
+        }
+        if(psico == "1" && idade < 18){
+            calma18++
+        }
+        entrevistado++
     }
+    println("Foram entrevistados $entrevistado pessoas!")
+    println("O numero de pessoas que se declararam calmas é de: $pessoaCalma")
+    println("O numero de mulheres que se declararam nervosas é de: $mulherNervosa")
+    println("O numero de homens que se declararam agressivos é de: $homemAgressivo")
+    println("O numero de pessoas que se declararam como Outro e se dizem calmo é de: $outrosCalmo")
+    println("O numero de pessoas que tem acima de 40 anos e se declaram nervosas é de: $nervosa40")
+    println("O numero de pessoas que tem abaixo de 18 anos e se declaram calmas é de: $calma18")
+
 }
